b91e8da636e210d7bfe03032bc96eb02d0718271
Abdul Bari <abdulbari80@gmail.com>
2025-10-05 14:46:37 +1100

    simple web application developed for user interaction

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+from src.component.user_data_prediction import ProcessUserData, Prediction
+
+application = Flask(__name__)
+app = application
+
+# add route for home page
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET', 'POST'])    
+def predict_user_data():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        user_data = ProcessUserData(
+            gender= request.form.get('gender'),
+            race_ethnicity= request.form.get('ethnicity'),
+            parental_level_of_education = request.form.get('parental_level_of_education'),
+            lunch = request.form.get('lunch'),
+            test_preparation_course = request.form.get('test_preparation_course'),
+            reading_score =  float(request.form.get('reading_score')),
+            writing_score = float(request.form.get('writing_score'))
+        )
+        df_user_input = user_data.get_data_frame()
+        prediction_obj = Prediction()
+        result = prediction_obj.get_prediction(df_user_input)
+        return render_template('home.html', results=round(result[0], 2))
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
